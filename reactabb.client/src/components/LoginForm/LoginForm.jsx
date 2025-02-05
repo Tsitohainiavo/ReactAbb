db9a711f73430670ceaf01ac4b1e501c3a9e3d14
Ny Avo Fisandratana <nyavotsitohaina@gmail.com>
2025-02-05 14:06:36 +0300

    login et redirection avec css oke

--- reactabb.client/src/components/LoginForm/LoginForm.jsx
+++ reactabb.client/src/components/LoginForm/LoginForm.jsx
@@ -3,38 +3,26 @@
 import { useState } from 'react';
 import './LoginForm.css';
 import { FaUser, FaLock } from 'react-icons/fa';
-import axios from 'axios'; // Importer Axios
+import axios from 'axios';
 
 const LoginForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = async (e) => {
-        e.preventDefault(); // Empêcher le rechargement de la page
-
-
+        e.preventDefault();
         try {
-            // Envoyer les données du formulaire au backend
             const response = await axios.post('https://localhost:7265/api/oracledata/login', {
                 Email: email,
                 Password: password,
             });
-
-            // Si la connexion est réussie
             if (response.status === 200) {
-                //alert("Utilisateur connecté"); // Afficher un message de succès
-                // Rediriger l'utilisateur ou stocker le token JWT ici
-                //console.log('Utilisateur connecté:', response.data.Utilisateur);
                 window.location.href = '/utilisateurtable';
-
             }
         } catch (error) {
-            // Gérer les erreurs
             if (error.response) {
-                // Erreur renvoyée par le backend
-                alert(error.response.data); // Afficher le message d'erreur
+                alert(error.response.data);
             } else {
-                // Erreur réseau ou autre
                 console.error('Erreur lors de la connexion:', error.message);
                 alert('Une erreur s\'est produite lors de la connexion.');
             }
@@ -42,41 +30,40 @@ const LoginForm = () => {
     };
 
     return (
-        <div className='wrapper'>
-            <form onSubmit={handleSubmit}>
-                <h1>Login</h1>
-                <div className='input-box'>
-                    <input
-                        type='text'
-                        placeholder='Email'
-                        required
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <FaUser className='icon' />
-                </div>
-                <div className='input-box'>
-                    <input
-                        type='password'
-                        placeholder='Mot de passe'
-                        required
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <FaLock className='icon' />
-                </div>
-
-                <div className='remember-forgot'>
-                    <label><input type='checkbox' /> Se souvenir de moi</label>
-                    <a>Mot de passe oublie ?</a>
-                </div>
-
-                <button type='submit'>Login</button>
-
-                <div className='register-link'>
-                    <p>Vous n'avez pas de compte?<a> Creez en un</a></p>
-                </div>
-            </form>
+        <div className="login-form-container">
+            <div className='wrapper'>
+                <form onSubmit={handleSubmit}>
+                    <h1>Login</h1>
+                    <div className='input-box'>
+                        <input
+                            type='text'
+                            placeholder='Email'
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <FaUser className='icon' />
+                    </div>
+                    <div className='input-box'>
+                        <input
+                            type='password'
+                            placeholder='Mot de passe'
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <FaLock className='icon' />
+                    </div>
+                    <div className='remember-forgot'>
+                        <label><input type='checkbox' /> Se souvenir de moi</label>
+                        <a>Mot de passe oublie ?</a>
+                    </div>
+                    <button type='submit'>Login</button>
+                    <div className='register-link'>
+                        <p>Vous n'avez pas de compte?<a> Creez en un</a></p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
